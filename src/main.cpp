dc4e4b4f29c14653243b6e3a0ae70f083c29efb7
Nejc Flegar <nejc.flegar.si@gmail.com>
2025-07-23 12:12:35 +0200

    nekiMauga

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,34 @@
 #include <Arduino.h>
 #include <TFT_eSPI.h>
+#include "mc.h"
 
 TFT_eSPI tft = TFT_eSPI();
+TFT_eSprite MainKnight = TFT_eSprite(&tft);
+TFT_eSprite background1 = TFT_eSprite(&tft);
+TFT_eSprite background2 = TFT_eSprite(&tft);
 
 void setup(){
   tft.init();
-  tft.fillScreen(TFT_GREENYELLOW);
+  tft.setRotation(3);
+  tft.fillScreen(TFT_YELLOW);
+
+  background1.createSprite(320,50);
+  background1.setSwapBytes(true);
+  background1.fillRect(0,0,320,50,TFT_DARKGREEN);
+
+  background2.createSprite(220,190);
+  background2.setSwapBytes(true);
+  background2.fillRect(0,0,320,190,TFT_CYAN);
+  
+
+  MainKnight.createSprite(16,16);
+  MainKnight.setSwapBytes(true);
+  MainKnight.pushImage(0,0,16,16,knight[0]);
 
 }
 
 void loop(){
-
-
+  MainKnight.pushToSprite(&background2,100,100,TFT_WHITE);
+  background1.pushSprite(0,190);
+  background2.pushSprite(0,0);
 }
